3d49fc311983785f3ef42b14cae623c7e3260c23
suhailmemon01101984 <suhailmemon84@gmail.com>
2024-12-12 20:11:21 -0700

    ratings-counter.py

--- ratings-counter.py
+++ ratings-counter.py
@@ -3,9 +3,12 @@ import collections
 
 sparkconfig=SparkConf().setMaster("local").setAppName("RatingsHistogram")
 sc=SparkContext(conf=sparkconfig)
+#sc.setLogLevel("ERROR")
+sc.setLogLevel("ERROR")
 lines=sc.textFile("/Users/suhailmemon/Documents/MACBOOKPRO/dell laptop/Desktop/git/udemy_bigdatapyspark/ml-100k/u.data")
 ratings=lines.map(lambda x:x.split()[2])
 result=ratings.countByValue()
 sortedResults=collections.OrderedDict(sorted(result.items()))
 for key,value in sortedResults.items():
     print(f"{key} {value}")
+quit()
