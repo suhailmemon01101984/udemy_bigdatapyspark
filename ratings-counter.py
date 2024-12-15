c0f3b1694f0d173a8f76bc5b742ce9c5ca580b4a
suhailmemon01101984 <suhailmemon84@gmail.com>
2024-12-15 14:53:04 -0700

    ratings-counter.py

--- ratings-counter.py
+++ ratings-counter.py
@@ -11,7 +11,7 @@ sc.setLogLevel("ERROR")
 #use the textfile method to. the textfile breaks up this u.data file line by line and every line corresponds to one value in the rdd.
 #so if your file u.data has 5 lines then your rdd will have 5 values with each value being the string that represents the whole line of text
 lines=sc.textFile("/Users/suhailmemon/Documents/MACBOOKPRO/dell laptop/Desktop/git/udemy_bigdatapyspark/ml-100k/u.data")
-#call the map function with input as the lines rdd where for each line you will split that line and then get the values in the 3rd column
+#call the map function with input as the lines rdd where for each line you will split that line by space and then get the values in the 3rd column
 ratings=lines.map(lambda x:x.split()[2])
 #call the countbyvalue function to count number of occurrences of each value
 result=ratings.countByValue()
