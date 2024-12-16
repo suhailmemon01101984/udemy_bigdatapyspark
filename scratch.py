95a593e2c19d004df724c951e5dbd034f2dfc21d
suhailmemon01101984 <suhailmemon84@gmail.com>
2024-12-15 18:49:30 -0700

    scratch.py and word-counter.py

--- scratch.py
+++ scratch.py
@@ -1,3 +1,11 @@
-s = '196	242	3	881250949'
-print(s.split())
-print(s.split()[2])
+import re
+
+text = "Hello World 123 Suhail"
+
+# Match word characters
+words = re.compile(r'\w+').split(text.lower())
+print(words) 
+
+# Match non-word characters
+non_words = re.compile(r'\W+').split(text.lower())
+print(non_words)
--- word-counter.py
+++ word-counter.py
@@ -1,14 +1,19 @@
+import re
+
 from pyspark import SparkConf, SparkContext
 
 sparkconfig=SparkConf().setMaster("local").setAppName("WordCount")
 sc=SparkContext(conf=sparkconfig)
 sc.setLogLevel("ERROR")
 
+def normalizeWords(text):
+    return re.compile(r'\W+',re.UNICODE).split(text.lower())
+
 input=sc.textFile("/Users/suhailmemon/Documents/MACBOOKPRO/dell laptop/Desktop/git/udemy_bigdatapyspark/datafiles/book")
-words=input.flatMap(lambda x: x.split())
+words=input.flatMap(normalizeWords)
 wordCounts=words.countByValue()
 for word,count in wordCounts.items():
     cleanword=word.encode('ascii','ignore')
     if(cleanword):
-        print(cleanword, count)
+        print(cleanword.decode(), count)
 quit()
