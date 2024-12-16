22674ae9c55c665b918e66d35d3cfd5185e23891
suhailmemon01101984 <suhailmemon84@gmail.com>
2024-12-15 19:47:41 -0700

    word-counter-sorted.py

--- scratch.py
+++ scratch.py
@@ -1,11 +1,24 @@
 import re
 
-text = "Hello World 123 Suhail"
+from pyspark import SparkConf, SparkContext
 
-# Match word characters
-words = re.compile(r'\w+').split(text.lower())
-print(words) 
+sparkconfig=SparkConf().setMaster("local").setAppName("WordCount")
+sc=SparkContext(conf=sparkconfig)
+sc.setLogLevel("ERROR")
 
-# Match non-word characters
-non_words = re.compile(r'\W+').split(text.lower())
-print(non_words)
+def normalizeWords(text):
+    return re.compile(r'\W+',re.UNICODE).split(text.lower()) #\W+ to remove all non word characters and use it as a splitting pattern
+
+input=sc.textFile("/Users/suhailmemon/Documents/MACBOOKPRO/dell laptop/Desktop/git/udemy_bigdatapyspark/datafiles/book")
+words=input.flatMap(normalizeWords)
+wordCounts=words.map(lambda x:(x,1)).reduceByKey(lambda x,y:x+y)
+wordCounts_temp=wordCounts.map(lambda x: (x[1], x[0]))
+wordCountsSorted=wordCounts_temp.sortByKey()
+#wordCounts_temp=wordCounts.map(lambda x,y:(y,x)).sortByKey()
+#wordCountsSorted = wordCounts.sortBy(lambda x: (x[1]), ascending=False).map(lambda x: (x[1], x[0]))
+results=wordCountsSorted.collect()
+for eachrow in results:
+    cleanword=eachrow[1].encode('ascii','ignore')
+    if(cleanword):
+        print(f'{cleanword.decode()}:        {eachrow[0]}') #this .decode() was added to change from binary format to string...so eg: from  b'mindful' to just mindful
+quit()
--- word-counter-sorted.py
+++ word-counter-sorted.py
@@ -12,11 +12,13 @@ def normalizeWords(text):
 input=sc.textFile("/Users/suhailmemon/Documents/MACBOOKPRO/dell laptop/Desktop/git/udemy_bigdatapyspark/datafiles/book")
 words=input.flatMap(normalizeWords)
 wordCounts=words.map(lambda x:(x,1)).reduceByKey(lambda x,y:x+y)
+wordCounts_flipped=wordCounts.map(lambda x: (x[1], x[0]))
+wordCountsSorted=wordCounts_flipped.sortByKey()
 #wordCounts_temp=wordCounts.map(lambda x,y:(y,x)).sortByKey()
-wordCountsSorted = wordCounts.sortBy(lambda x: (x[1]), ascending=False).map(lambda x: (x[1], x[0]))
+#wordCountsSorted = wordCounts.sortBy(lambda x: (x[1]), ascending=False).map(lambda x: (x[1], x[0]))
 results=wordCountsSorted.collect()
 for eachrow in results:
     cleanword=eachrow[1].encode('ascii','ignore')
     if(cleanword):
-        print(cleanword.decode(), eachrow[0]) #this .decode() was added to change from binary format to string...so eg: from  b'mindful' to just mindful
+        print(f'{cleanword.decode()}:        {eachrow[0]}') #this .decode() was added to change from binary format to string...so eg: from  b'mindful' to just mindful
 quit()
