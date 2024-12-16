c23d3f6d2201550ede7495707df991fd215a3c24
suhailmemon01101984 <suhailmemon84@gmail.com>
2024-12-15 20:18:47 -0700

    spend-by-customer.py

--- spend-by-customer.py
+++ spend-by-customer.py
@@ -13,7 +13,7 @@ def parseline(line):
 
 lines=sc.textFile("/Users/suhailmemon/Documents/MACBOOKPRO/dell laptop/Desktop/git/udemy_bigdatapyspark/datafiles/customer-orders.csv")
 customerid_n_spendamounts=lines.map(parseline)
-total_spend_by_customer=customerid_n_spendamounts.reduceByKey(lambda x,y:x+y)
+total_spend_by_customer=customerid_n_spendamounts.reduceByKey(lambda x,y:x+y).sortBy(lambda x:x[1])
 results=total_spend_by_customer.collect()
 for eachrow in results:
     print(f'{eachrow[0]},{round(eachrow[1],2)}')
