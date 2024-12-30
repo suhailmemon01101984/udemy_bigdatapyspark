9ac618119d2294a8da4357c3223ef0e7b1b37869
suhailmemon01101984 <suhailmemon84@gmail.com>
2024-12-29 20:29:18 -0700

    friends-by-age-dataframe.py

--- friends-by-age-dataframe.py
+++ friends-by-age-dataframe.py
@@ -17,9 +17,9 @@ friendsbyage.groupBy("age").agg(func.round(func.avg("friends"),2)).sort("age").s
 friendsbyage.groupBy("age").agg(func.round(func.avg("friends"),2)).alias("friends_avg").sort("age").show()
 
 #doing same code with sql after creating a view below
-# peopleDF.createOrReplaceTempView("people_header_vw")
-# avg_friends_by_age=sparkSessn.sql("select age, avg(friends) from people_header_vw group by 1")
-# for eachrow in avg_friends_by_age.collect():
-#     print(eachrow)
+peopleDF.createOrReplaceTempView("people_header_vw")
+avg_friends_by_age=sparkSessn.sql("select age, round(avg(friends),2) as friends_avg from people_header_vw group by 1 order by 1")
+for eachrow in avg_friends_by_age.collect():
+    print(eachrow[0],eachrow[1])
 
 sparkSessn.stop()
