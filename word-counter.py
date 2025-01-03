b7671952f6114f93d0669cdb6173c07fb34fa6f4
suhailmemon01101984 <suhailmemon84@gmail.com>
2025-01-03 11:53:13 -0700

    word-counter.py

--- word-counter.py
+++ word-counter.py
@@ -7,7 +7,7 @@ sc=SparkContext(conf=sparkconfig)
 sc.setLogLevel("ERROR")
 
 def normalizeWords(text):
-    return re.compile(r'\W+',re.UNICODE).split(text.lower()) #\W+ to remove all non word characters and use it as a splitting pattern
+    return re.compile(r'\W+',re.UNICODE).split(text.lower()) #re.compile(r'\W+', re.UNICODE) compiles a regular expression pattern for matching non-word characters. \W+ matches one or more non-word characters (i.e., anything that isn't a letter, number, or underscore). re.UNICODE ensures that the regex operations respect Unicode characters, making it work correctly for characters beyond the standard ASCII set. The split() function splits the text (now in lowercase) wherever the regex pattern (\W+, one or more non-word characters) matches
 
 input=sc.textFile("/Users/suhailmemon/Documents/MACBOOKPRO/dell laptop/Desktop/git/udemy_bigdatapyspark/datafiles/book")
 words=input.flatMap(normalizeWords)
