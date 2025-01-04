4c8399efc8e5dd3eee07ce4b7980a427d35f687d
suhailmemon01101984 <suhailmemon84@gmail.com>
2025-01-03 17:59:52 -0700

    word-count-better-sorted-dataframe.py

--- word-count-better-sorted-dataframe.py
+++ word-count-better-sorted-dataframe.py
@@ -11,3 +11,5 @@ lowerCaseWords=wordsWithoutEmptyString.select(func.lower(wordsWithoutEmptyString
 wordCounts=lowerCaseWords.groupBy("word").count()
 wordCountsSorted=wordCounts.sort("count", ascending=False)# add ,ascending=False to sort by descending. else remove this portion to show order in ascending which is the default
 wordCountsSorted.show(wordCountsSorted.count()) #by default the show method will only display top 20 rows. to make it show every row you pass the parameter which is the count of the whole dataframe: wordCountsSorted
+
+sparkSessn.stop()
