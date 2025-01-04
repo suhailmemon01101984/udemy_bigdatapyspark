fcb116cb243ad3cc4a1022627ccaaf7756592595
suhailmemon01101984 <suhailmemon84@gmail.com>
2025-01-03 18:20:33 -0700

    min-temperatures-dataframe.py

--- min-temperatures-dataframe.py
+++ min-temperatures-dataframe.py
@@ -5,10 +5,25 @@ from pyspark.sql.types import StructType, StructField, StringType, IntegerType, 
 sparkSessn=SparkSession.builder.appName("MinTemperatures").getOrCreate()
 
 #true means column allows nulls
-temperatures_schema=StructType[StructField("stationID", StringType, True), StructField("date", IntegerType, True), StructField("measure_type", StringType, True), StructField("temperature", FloatType, True)]
+temperatures_schema=StructType([StructField("stationID", StringType(), True), StructField("date", IntegerType(), True), StructField("measure_type", StringType(), True), StructField("temperature", FloatType(), True)])
 
 df=sparkSessn.read.schema(temperatures_schema).csv("/Users/suhailmemon/Documents/MACBOOKPRO/dell laptop/Desktop/git/udemy_bigdatapyspark/datafiles/1800.csv")
 
 df.printSchema()
 
+onlymintemps=df.filter(df.measure_type=="TMIN")
 
+station_n_temps=onlymintemps.select("stationID","temperature")
+
+minTempsByStation=station_n_temps.groupBy("stationID").min("temperature")
+
+minTempsByStationF=minTempsByStation.withColumn("Farenheit_temp", func.round(func.col("min(temperature)")*0.1*(9.0/5.0)+32, 2)).select("stationID", "Farenheit_temp").sort("Farenheit_temp")
+
+minTempsByStationF.printSchema()
+
+finalresults=minTempsByStationF.collect()
+
+for result in finalresults:
+    print(f"{result[0]}    {result[1]}F")
+
+sparkSessn.stop()
