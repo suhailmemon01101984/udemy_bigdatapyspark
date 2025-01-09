02f2f5bdcc610be34de3a326f2651dd61bde9053
suhailmemon01101984 <suhailmemon84@gmail.com>
2025-01-08 21:15:34 -0700

    most-popular-superhero-dataframe.py

---
+++ most-obscure-superhero-dataframe.py
@@ -0,0 +1,39 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as func
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+
+sparkSessn=SparkSession.builder.appName("MostObscureSuperhero").getOrCreate()
+
+marvelNameSchema=StructType([StructField("id", IntegerType(), True), \
+                             StructField("name", StringType(), True) \
+                             ])
+
+marvelNames=sparkSessn.read.schema(marvelNameSchema).option("sep", " ").csv("/Users/suhailmemon/Documents/MACBOOKPRO/dell laptop/Desktop/git/udemy_bigdatapyspark/datafiles/marvel-names")
+
+lines=sparkSessn.read.text("/Users/suhailmemon/Documents/MACBOOKPRO/dell laptop/Desktop/git/udemy_bigdatapyspark/datafiles/marvel-graph")
+
+superHeroConnectionsStage=lines.withColumn("id", func.split(func.col("value"), " ")[0]) \
+    .withColumn("connections", func.size(func.split(func.col("value"), " "))-1)
+
+superHeroConnectionsStage.show()
+
+superHeroConnections=superHeroConnectionsStage.groupBy("id").agg(func.sum("connections").alias("totalConnections")).orderBy("totalConnections")
+
+superHeroConnections.show()
+
+superHeroSingleConnectionStage=superHeroConnections.filter(superHeroConnections.totalConnections=="1")
+
+superHeroSingleConnectionStage.show()
+
+superHeroSingleConnection=superHeroSingleConnectionStage.join(marvelNames,"id").select("name", "totalConnections")
+
+superHeroSingleConnection.show(superHeroSingleConnection.count())
+
+superHeroZeroConnectionStage=superHeroConnections.filter(superHeroConnections.totalConnections=="0")
+superHeroZeroConnectionStage.show()
+
+superHeroZeroConnection=superHeroZeroConnectionStage.join(marvelNames,"id").select("name", "totalConnections")
+
+superHeroZeroConnection.show(superHeroZeroConnection.count())
+
+sparkSessn.stop()
