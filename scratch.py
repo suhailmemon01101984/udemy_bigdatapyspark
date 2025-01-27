43612adb8b279627b98388752c08ee4981e6d30f
suhailmemon01101984 <suhailmemon84@gmail.com>
2025-01-26 21:27:57 -0700

    scratch.py

--- scratch.py
+++ scratch.py
@@ -1,17 +1,13 @@
 from pyspark import SparkConf, SparkContext
 import collections
 
-sparkconfig=SparkConf().setMaster("local").setAppName("RatingsCounter")
+sparkconfig=SparkConf().setMaster("local").setAppName("RatingCounter")
 sc=SparkContext(conf=sparkconfig)
 
+
 lines=sc.textFile("/Users/suhailmemon/Documents/MACBOOKPRO/dell laptop/Desktop/git/udemy_bigdatapyspark/datafiles/ml-100k/u.data")
-
-ratings=lines.map(lambda x: x.split()[2])
-
+ratings=lines.map(lambda x:x.split()[2])
 results=ratings.countByValue()
-
 sortedresults=collections.OrderedDict(sorted(results.items()))
-
 for key,value in sortedresults.items():
     print(key,value)
-
