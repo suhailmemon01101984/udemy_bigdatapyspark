2c9faa2a342b82a4ef875bf0d8375bb5a0dd9fea
suhailmemon01101984 <suhailmemon84@gmail.com>
2025-01-27 18:42:58 -0700

    scratch.py

--- scratch.py
+++ scratch.py
@@ -1,13 +1,21 @@
 from pyspark import SparkConf, SparkContext
-import collections
 
-sparkconfig=SparkConf().setMaster("local").setAppName("RatingCounter")
+sparkconfig=SparkConf().setMaster("local").setAppName("FriendsByAge")
 sc=SparkContext(conf=sparkconfig)
 
 
-lines=sc.textFile("/Users/suhailmemon/Documents/MACBOOKPRO/dell laptop/Desktop/git/udemy_bigdatapyspark/datafiles/ml-100k/u.data")
-ratings=lines.map(lambda x:x.split()[2])
-results=ratings.countByValue()
-sortedresults=collections.OrderedDict(sorted(results.items()))
-for key,value in sortedresults.items():
-    print(key,value)
+def parseline(line):
+    fields=line.split(',')
+    age=int(fields[2])
+    num_friends=int(fields[3])
+    return age,num_friends
+
+lines=sc.textFile("/Users/suhailmemon/Documents/MACBOOKPRO/dell laptop/Desktop/git/udemy_bigdatapyspark/datafiles/fakefriends.csv")
+age_n_friends=lines.map(parseline)
+age_sum_friends_n_count=age_n_friends.mapValues(lambda x: (x,1)).reduceByKey(lambda x,y: x+y)
+avg_age_by_friends=age_sum_friends_n_count.mapValues(lambda x:x[0]/x[1])
+results=avg_age_by_friends.collect()
+for row in results:
+    print(row)
+
+quit()
