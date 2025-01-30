ea584e49d88d197f0be50639a8b62a0c9beb7131
suhailmemon01101984 <suhailmemon84@gmail.com>
2025-01-29 19:43:06 -0700

    scratch.py

--- scratch.py
+++ scratch.py
@@ -1,25 +1,25 @@
 from pyspark import SparkConf, SparkContext
 
-sparkconfig=SparkConf().setMaster("local").setAppName("MinTemps")
+sparkconfig=SparkConf().setMaster("local").setAppName("MaxTemps")
 sc=SparkContext(conf=sparkconfig)
 sc.setLogLevel("ERROR")
-
 
 def parseline(line):
     fields=line.split(',')
     stationID=fields[0]
     entryType=fields[2]
-    tempf=(float(fields[3])*0.1*(9.0/5.0))+32.0
-    return stationID,entryType,tempf
+    tempF=(float(fields[3])*0.1*(9.0/5.0))+32
+    return stationID, entryType, tempF
 
-lines=sc.textFile('/Users/suhailmemon/Documents/MACBOOKPRO/dell laptop/Desktop/git/udemy_bigdatapyspark/datafiles/1800.csv')
-station_type_temps=lines.map(parseline)
-min_station_type_temps=station_type_temps.filter(lambda x: "TMAX" in x[1])
-station_n_temps=min_station_type_temps.map(lambda x: (x[0],x[2]))
-min_temp_by_station=station_n_temps.reduceByKey(lambda x,y:max(x,y))
 
-results=min_temp_by_station.collect()
-for row in results:
-    print(f'{row[0]}    {round(row[1],2)}F')
+lines=sc.textFile("/Users/suhailmemon/Documents/MACBOOKPRO/dell laptop/Desktop/git/udemy_bigdatapyspark/datafiles/1800.csv")
+station_temptype_temps=lines.map(parseline)
+only_max_temps=station_temptype_temps.filter(lambda x: 'TMAX' in x[1])
+station_temps=only_max_temps.map(lambda x: (x[0],x[2]))
+max_temp_by_station=station_temps.reduceByKey(lambda x,y: max(x,y))
+
+results=max_temp_by_station.collect()
+for result in results:
+    print(f'{result[0]}\t{round(result[1],2)}F')
 
 quit()
