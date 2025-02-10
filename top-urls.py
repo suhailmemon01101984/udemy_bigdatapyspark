4a1b49f3c9bb99f817607ff22868d741108a3641
suhailmemon01101984 <suhailmemon84@gmail.com>
2025-02-09 17:01:44 -0700

    top-urls.py

--- top-urls.py
+++ top-urls.py
@@ -1,13 +1,11 @@
 ###run this code and copy the access_log.txt into the /Users/suhailmemon/Downloads/logs directory and then do another file and do another file and see how the status counts change
 
-from pyspark import SparkContext
-from pyspark.streaming import StreamingContext
-from pyspark.sql import Row, SparkSession
+from pyspark.sql import SparkSession
 
-from pyspark.sql.functions import regexp_extract
+import pyspark.sql.functions as func
 
 # Create a SparkSession (the config bit is only for Windows!)
-sparkconfig = SparkSession.builder.appName("StructuredStreaming").getOrCreate()
+sparkconfig = SparkSession.builder.appName("TopUrls").getOrCreate()
 
 # Monitor the logs directory for new log data, and read in the raw lines as accessLines
 accessLines = sparkconfig.readStream.text("/Users/suhailmemon/Downloads/logs")
@@ -19,17 +17,19 @@ generalExp = r'\"(\S+)\s(\S+)\s*(\S*)\"'
 timeExp = r'\[(\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2} -\d{4})]'
 hostExp = r'(^\S+\.[\S+\.]+\S+)\s'
 
-logsDF = accessLines.select(regexp_extract('value', hostExp, 1).alias('host'),
-                         regexp_extract('value', timeExp, 1).alias('timestamp'),
-                         regexp_extract('value', generalExp, 1).alias('method'),
-                         regexp_extract('value', generalExp, 2).alias('endpoint'),
-                         regexp_extract('value', generalExp, 3).alias('protocol'),
-                         regexp_extract('value', statusExp, 1).cast('integer').alias('status'),
-                         regexp_extract('value', contentSizeExp, 1).cast('integer').alias('content_size'))
+logsDF = accessLines.select(func.regexp_extract('value', hostExp, 1).alias('host'),
+                         func.regexp_extract('value', timeExp, 1).alias('timestamp'),
+                         func.regexp_extract('value', generalExp, 1).alias('method'),
+                         func.regexp_extract('value', generalExp, 2).alias('endpoint'),
+                         func.regexp_extract('value', generalExp, 3).alias('protocol'),
+                         func.regexp_extract('value', statusExp, 1).cast('integer').alias('status'),
+                         func.regexp_extract('value', contentSizeExp, 1).cast('integer').alias('content_size'))
 
-# Keep a running count of every access by status code
-statusCountsDF = logsDF.groupBy(logsDF.status).count()
+logsDF2= logsDF.withColumn("eventTime", func.current_timestamp())
 
+endpointCounts=logsDF2.groupBy(func.window(func.col("eventTime"),windowDuration="30 seconds", slideDuration="10 seconds"), func.col("endpoint")).count()
+
+sortedEndpointCounts=endpointCounts.orderBy(func.col("count").desc())
 # Give the query a name: counts, Kick off our streaming query with the .start() call , dumping complete results to the console using outputMode("complete").format("console")
 query = ( statusCountsDF.writeStream.outputMode("complete").format("console").queryName("counts").start() )
 
