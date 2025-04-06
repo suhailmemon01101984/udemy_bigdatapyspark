c7ba226157b38ad0939a402cc271ebe31e1f1500
suhailmemon01101984 <suhailmemon84@gmail.com>
2025-04-06 17:20:32 -0600

    scratch.py

--- scratch.py
+++ scratch.py
@@ -1,29 +1,31 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as func
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
 
-sparkSessn=SparkSession.builder.appName("WordCounts").getOrCreate()
-inputDF=sparkSessn.read.text("/Users/suhailmemon/Documents/MACBOOKPRO/dell laptop/Desktop/git/udemy_bigdatapyspark/datafiles/book")
+sparkSessn=SparkSession.builder.appName("MinTemps").getOrCreate()
+
+temperaturesSchema=StructType([
+ StructField("stationID", StringType(), True), \
+    StructField("date", IntegerType(), True), \
+    StructField("measureType", StringType(), True), \
+    StructField("temperature", FloatType(), True) \
+])
+
+inputDF=sparkSessn.read.schema(temperaturesSchema).csv("/Users/suhailmemon/Documents/MACBOOKPRO/dell laptop/Desktop/git/udemy_bigdatapyspark/datafiles/1800.csv")
 inputDF.printSchema()
+inputDF.show()
 
-wordsDF=inputDF.select(func.explode(func.split(inputDF.value,"\\W+")).alias("word"))
+minTempsDF=inputDF.filter(inputDF.measureType=="TMIN")
+stationTempsDF=minTempsDF.select("stationID","temperature")
+minTempsByStationDF=stationTempsDF.groupBy("stationID").agg(func.min("temperature").alias("minTemp"))
+minTempsByStationFarenheitDF=minTempsByStationDF.withColumn("MinFarenheitTemp",func.round(func.col("minTemp")*0.1*(9.0/5.0)+32,2)).select("stationID","MinFarenheitTemp").sort("MinFarenheitTemp",ascending=True)
+minTempsByStationFarenheitDF.show()
+minTempsByStationFarenheitDF.printSchema()
 
-wordsDF.printSchema()
+finalResults=minTempsByStationFarenheitDF.collect()
+print(type(finalResults))
 
-print(type(wordsDF))
-
-
-wordsWithoutEmptyStringDF=wordsDF.filter(wordsDF.word!="")
-
-lowercaseWordsDF=wordsWithoutEmptyStringDF.select(func.lower(wordsWithoutEmptyStringDF.word).alias("word"))
-
-lowercaseWordsDF.printSchema()
-
-wordCountsDF=lowercaseWordsDF.groupBy("word").count()
-
-print(type(wordCountsDF))
-
-wordCountsSortedDF=wordCountsDF.sort("count",ascending=False)
-
-wordCountsSortedDF.show(wordCountsSortedDF.count())
+for result in finalResults:
+    print(f"{result[0]}    {result[1]}F")
 
 sparkSessn.stop()
