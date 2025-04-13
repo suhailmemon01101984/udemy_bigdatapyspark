fbe14cc2ecaa7cc9bfa3591e91d9c97223d25feb
suhailmemon01101984 <suhailmemon84@gmail.com>
2025-04-13 15:52:46 -0600

    scratch.py

--- scratch.py
+++ scratch.py
@@ -1,31 +1,21 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as func
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
+from pyspark.sql.types import StructType, StructField, IntegerType, LongType
 
-sparkSessn=SparkSession.builder.appName("MinTemps").getOrCreate()
+sparkSessn=SparkSession.builder.appName("PopularMovies").getOrCreate()
 
-temperaturesSchema=StructType([
- StructField("stationID", StringType(), True), \
-    StructField("date", IntegerType(), True), \
-    StructField("measureType", StringType(), True), \
-    StructField("temperature", FloatType(), True) \
-])
+movieSchema=StructType([StructField("userID",IntegerType(),True), \
+                        StructField("movieID",IntegerType(),True), \
+                        StructField("rating",IntegerType(),True), \
+                        StructField("timestamp",LongType(),True) \
+                        ])
 
-inputDF=sparkSessn.read.schema(temperaturesSchema).csv("/Users/suhailmemon/Documents/MACBOOKPRO/dell laptop/Desktop/git/udemy_bigdatapyspark/datafiles/1800.csv")
-inputDF.printSchema()
-inputDF.show()
+moviesDF=sparkSessn.read.option("sep","\t").schema(movieSchema).csv("/Users/suhailmemon/Documents/MACBOOKPRO/dell laptop/Desktop/git/udemy_bigdatapyspark/datafiles/ml-100k/u.data")
 
-minTempsDF=inputDF.filter(inputDF.measureType=="TMIN")
-stationTempsDF=minTempsDF.select("stationID","temperature")
-minTempsByStationDF=stationTempsDF.groupBy("stationID").agg(func.min("temperature").alias("minTemp"))
-minTempsByStationFarenheitDF=minTempsByStationDF.withColumn("MinFarenheitTemp",func.round(func.col("minTemp")*0.1*(9.0/5.0)+32,2)).select("stationID","MinFarenheitTemp").sort("MinFarenheitTemp",ascending=True)
-minTempsByStationFarenheitDF.show()
-minTempsByStationFarenheitDF.printSchema()
+moviesDF.printSchema()
 
-finalResults=minTempsByStationFarenheitDF.collect()
-print(type(finalResults))
+topMoviesDF=moviesDF.groupBy("movieID").count().orderBy(func.desc("count"))
 
-for result in finalResults:
-    print(f"{result[0]}    {result[1]}F")
+topMoviesDF.show()
 
 sparkSessn.stop()
